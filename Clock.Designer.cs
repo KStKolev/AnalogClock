26b067304057c3c36777dcf2de74f0c15f895a97
Kolio Kolev <koliokolev301@gmail.com>
2024-10-26 17:51:31 +0300

    Add project files.

---
+++ Clock.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace AnalogClock
+{
+    partial class Clock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Clock));
+            this.digitalClockBG = new System.Windows.Forms.Label();
+            this.firstCountryZone = new System.Windows.Forms.Label();
+            this.digitalClockItaly = new System.Windows.Forms.Label();
+            this.firstCountryName = new System.Windows.Forms.Label();
+            this.firstCountryTime = new System.Windows.Forms.Label();
+            this.secondCountryName = new System.Windows.Forms.Label();
+            this.secondCountryTime = new System.Windows.Forms.Label();
+            this.secondCountryZone = new System.Windows.Forms.Label();
+            this.secondsDigClockItaly = new System.Windows.Forms.Label();
+            this.secondsDigClockBG = new System.Windows.Forms.Label();
+            this.bulgariaClockBox = new System.Windows.Forms.PictureBox();
+            this.italyClockBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bulgariaClockBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.italyClockBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // digitalClockBG
+            // 
+            this.digitalClockBG.AutoSize = true;
+            this.digitalClockBG.BackColor = System.Drawing.Color.Black;
+            this.digitalClockBG.Font = new System.Drawing.Font("Bookman Old Style", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.digitalClockBG.ForeColor = System.Drawing.SystemColors.Info;
+            this.digitalClockBG.Location = new System.Drawing.Point(360, 530);
+            this.digitalClockBG.Name = "digitalClockBG";
+            this.digitalClockBG.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.digitalClockBG.Size = new System.Drawing.Size(0, 50);
+            this.digitalClockBG.TabIndex = 1;
+            // 
+            // firstCountryZone
+            // 
+            this.firstCountryZone.AutoSize = true;
+            this.firstCountryZone.Font = new System.Drawing.Font("Bookman Old Style", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.firstCountryZone.ForeColor = System.Drawing.Color.Red;
+            this.firstCountryZone.Location = new System.Drawing.Point(431, 45);
+            this.firstCountryZone.Name = "firstCountryZone";
+            this.firstCountryZone.Size = new System.Drawing.Size(0, 32);
+            this.firstCountryZone.TabIndex = 3;
+            // 
+            // digitalClockItaly
+            // 
+            this.digitalClockItaly.AutoSize = true;
+            this.digitalClockItaly.BackColor = System.Drawing.Color.Black;
+            this.digitalClockItaly.Font = new System.Drawing.Font("Bookman Old Style", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.digitalClockItaly.ForeColor = System.Drawing.SystemColors.Info;
+            this.digitalClockItaly.Location = new System.Drawing.Point(1058, 530);
+            this.digitalClockItaly.Name = "digitalClockItaly";
+            this.digitalClockItaly.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.digitalClockItaly.Size = new System.Drawing.Size(0, 50);
+            this.digitalClockItaly.TabIndex = 1;
+            // 
+            // firstCountryName
+            // 
+            this.firstCountryName.AutoSize = true;
+            this.firstCountryName.Font = new System.Drawing.Font("Bookman Old Style", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.firstCountryName.ForeColor = System.Drawing.Color.Black;
+            this.firstCountryName.Location = new System.Drawing.Point(206, 45);
+            this.firstCountryName.Name = "firstCountryName";
+            this.firstCountryName.Size = new System.Drawing.Size(0, 32);
+            this.firstCountryName.TabIndex = 7;
+            // 
+            // firstCountryTime
+            // 
+            this.firstCountryTime.AutoSize = true;
+            this.firstCountryTime.Font = new System.Drawing.Font("Bookman Old Style", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.firstCountryTime.ForeColor = System.Drawing.Color.Green;
+            this.firstCountryTime.Location = new System.Drawing.Point(354, 45);
+            this.firstCountryTime.Name = "firstCountryTime";
+            this.firstCountryTime.Size = new System.Drawing.Size(0, 32);
+            this.firstCountryTime.TabIndex = 8;
+            // 
+            // secondCountryName
+            // 
+            this.secondCountryName.AutoSize = true;
+            this.secondCountryName.Font = new System.Drawing.Font("Bookman Old Style", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.secondCountryName.ForeColor = System.Drawing.Color.Green;
+            this.secondCountryName.Location = new System.Drawing.Point(858, 45);
+            this.secondCountryName.Name = "secondCountryName";
+            this.secondCountryName.Size = new System.Drawing.Size(0, 32);
+            this.secondCountryName.TabIndex = 9;
+            // 
+            // secondCountryTime
+            // 
+            this.secondCountryTime.AutoSize = true;
+            this.secondCountryTime.Font = new System.Drawing.Font("Bookman Old Style", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.secondCountryTime.ForeColor = System.Drawing.Color.Black;
+            this.secondCountryTime.Location = new System.Drawing.Point(967, 45);
+            this.secondCountryTime.Name = "secondCountryTime";
+            this.secondCountryTime.Size = new System.Drawing.Size(0, 32);
+            this.secondCountryTime.TabIndex = 10;
+            // 
+            // secondCountryZone
+            // 
+            this.secondCountryZone.AutoSize = true;
+            this.secondCountryZone.Font = new System.Drawing.Font("Bookman Old Style", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.secondCountryZone.ForeColor = System.Drawing.Color.Red;
+            this.secondCountryZone.Location = new System.Drawing.Point(1049, 45);
+            this.secondCountryZone.Name = "secondCountryZone";
+            this.secondCountryZone.Size = new System.Drawing.Size(0, 32);
+            this.secondCountryZone.TabIndex = 11;
+            // 
+            // secondsDigClockItaly
+            // 
+            this.secondsDigClockItaly.AutoSize = true;
+            this.secondsDigClockItaly.BackColor = System.Drawing.Color.Black;
+            this.secondsDigClockItaly.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.secondsDigClockItaly.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.secondsDigClockItaly.ForeColor = System.Drawing.SystemColors.Info;
+            this.secondsDigClockItaly.Location = new System.Drawing.Point(1199, 550);
+            this.secondsDigClockItaly.Name = "secondsDigClockItaly";
+            this.secondsDigClockItaly.Size = new System.Drawing.Size(2, 30);
+            this.secondsDigClockItaly.TabIndex = 6;
+            // 
+            // secondsDigClockBG
+            // 
+            this.secondsDigClockBG.AutoSize = true;
+            this.secondsDigClockBG.BackColor = System.Drawing.Color.Black;
+            this.secondsDigClockBG.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.secondsDigClockBG.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.secondsDigClockBG.ForeColor = System.Drawing.SystemColors.Info;
+            this.secondsDigClockBG.Location = new System.Drawing.Point(502, 550);
+            this.secondsDigClockBG.Name = "secondsDigClockBG";
+            this.secondsDigClockBG.Size = new System.Drawing.Size(2, 30);
+            this.secondsDigClockBG.TabIndex = 2;
+            // 
+            // bulgariaClockBox
+            // 
+            this.bulgariaClockBox.BackColor = System.Drawing.Color.Transparent;
+            this.bulgariaClockBox.Location = new System.Drawing.Point(156, 105);
+            this.bulgariaClockBox.Name = "bulgariaClockBox";
+            this.bulgariaClockBox.Size = new System.Drawing.Size(478, 441);
+            this.bulgariaClockBox.TabIndex = 0;
+            this.bulgariaClockBox.TabStop = false;
+            // 
+            // italyClockBox
+            // 
+            this.italyClockBox.Location = new System.Drawing.Point(796, 105);
+            this.italyClockBox.Name = "italyClockBox";
+            this.italyClockBox.Size = new System.Drawing.Size(478, 441);
+            this.italyClockBox.TabIndex = 4;
+            this.italyClockBox.TabStop = false;
+            // 
+            // Clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1301, 663);
+            this.Controls.Add(this.secondCountryZone);
+            this.Controls.Add(this.secondCountryTime);
+            this.Controls.Add(this.secondCountryName);
+            this.Controls.Add(this.firstCountryTime);
+            this.Controls.Add(this.firstCountryName);
+            this.Controls.Add(this.secondsDigClockItaly);
+            this.Controls.Add(this.digitalClockItaly);
+            this.Controls.Add(this.italyClockBox);
+            this.Controls.Add(this.firstCountryZone);
+            this.Controls.Add(this.secondsDigClockBG);
+            this.Controls.Add(this.digitalClockBG);
+            this.Controls.Add(this.bulgariaClockBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Clock";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Analog Clock";
+            this.Load += new System.EventHandler(this.LoadClock);
+            ((System.ComponentModel.ISupportInitialize)(this.bulgariaClockBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.italyClockBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label digitalClockBG;
+        private System.Windows.Forms.Label firstCountryZone;
+        private System.Windows.Forms.Label digitalClockItaly;
+        private System.Windows.Forms.Label firstCountryName;
+        private System.Windows.Forms.Label firstCountryTime;
+        private System.Windows.Forms.Label secondCountryName;
+        private System.Windows.Forms.Label secondCountryTime;
+        private System.Windows.Forms.Label secondCountryZone;
+        private System.Windows.Forms.Label secondsDigClockItaly;
+        private System.Windows.Forms.Label secondsDigClockBG;
+        private System.Windows.Forms.PictureBox bulgariaClockBox;
+        private System.Windows.Forms.PictureBox italyClockBox;
+    }
+}
+
---
+++ Clock.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Text;
+
+namespace AnalogClock
+{
+
+    public partial class Clock : Form
+    {
+        // create a timer
+        Timer timer = new Timer();
+        // height and width of the clock
+        private int width = 400;
+        private int height = 400;
+        // hand values
+        private int secondHand = 140;
+        private int minuteHand = 110;
+        private int hourHand = 80;
+        // center point
+        private PointF center;
+        private Bitmap bulgarianImage;
+        private Bitmap italianImage;
+        private Graphics graphic;
+        // Clock size
+        private float size;
+        private float radius;
+
+        public Clock()
+        {
+            InitializeComponent();
+            // set value to the clock size variables.
+            size = height * 0.99f / 2;
+            radius = width / 2;
+            // set value to create a center for the clock.
+            center = new PointF(width / 2, height / 2);
+        }
+
+        // Method for loading the clock.
+        private void LoadClock(object sender, EventArgs e)
+        {
+            // create a new bitmap  
+            bulgarianImage = new Bitmap(width + 1, height + 1);
+            italianImage = new Bitmap(width + 1, height + 1);
+            //backcolor  
+            this.BackColor = Color.LightCyan;
+            //timer  
+            timer.Interval = 1000; // Tick in a second  
+            timer.Tick += new EventHandler(this.TimerTick);
+            timer.Start();
+        }
+
+        // Method timer from current time.
+        private void TimerTick(object sender, EventArgs e)
+        {
+            //get time  
+            int seconds = DateTime.Now.Second;
+            int minutes = DateTime.Now.Minute;
+            int hours = DateTime.Now.Hour;
+            //load the bitmap image  
+            bulgariaClockBox.Image = 
+                CreateClockImage(hours, minutes, seconds, bulgarianImage);
+            italyClockBox.Image =
+                CreateClockImage(hours - 1, minutes, seconds, italianImage);
+            // garbage collector
+            graphic.Dispose();
+        }
+
+        private Bitmap CreateClockImage(int hours, int minutes, 
+            int seconds, Bitmap chosenImage)
+        {
+            // create an image  
+            graphic = Graphics.FromImage(chosenImage);
+            graphic.Clear(Color.LightCyan);
+            if (hours == DateTime.Now.Hour)
+            {
+                Color[] surroundFillEllipse = new Color[] { Color.Wheat };
+                GradientBrushFilling(hours, minutes, seconds, 
+                    Color.AntiqueWhite, surroundFillEllipse, Color.RosyBrown,
+                    Color.DarkGoldenrod, Color.SandyBrown);
+            }
+            else
+            {
+                Color[] surroundFillEllipse = new Color[] { Color.OldLace };
+                GradientBrushFilling(hours, minutes, seconds, 
+                    Color.AntiqueWhite, surroundFillEllipse, Color.RosyBrown,
+                    Color.SandyBrown, Color.RosyBrown);
+            }
+            // Use manually created, void method - DigitalClock.
+            DigitalClock(seconds, minutes, hours + 1);
+            // Print the text for Time zone.
+            PrintTimeZoneText();
+            return chosenImage;
+        }
+
+        private void GradientBrushFilling(int hours, int minutes, int seconds, 
+            Color centerFillEllipse, Color[] surroundFillEllipse,
+            Color ellipseSurrondLines, Color ellipseSurroundrectangles, 
+            Color ellipseColor)
+        {
+            //Fill the clock with gradient brush
+            PathGradientBrush pathGradientBrush = 
+                GradientBrushFill(centerFillEllipse, surroundFillEllipse);
+            graphic.FillEllipse(pathGradientBrush, 0, 0, width, height);
+            // Use manually created, void method - PropertyLines()
+            PropertyLines(ellipseSurrondLines, ellipseSurroundrectangles);
+            int[] handCoordinationArray = new int[2];
+            // set value to the array variable with DrawAnalogClock()
+            handCoordinationArray = 
+                DrawAnalogClock(seconds, minutes, hours, ellipseColor);
+        }
+
+        private int[] DrawAnalogClock(int seconds, int minutes, 
+            int hours, Color ellipseColor)
+        {
+            if (hours == DateTime.Now.Hour)
+            {
+                // Bulgarian colors.
+                DrawRomanNumerals(ellipseColor, Brushes.White, 
+                    Brushes.Green, Brushes.Red);
+            }
+            else
+            {
+                // Italian colors.
+                DrawRomanNumerals(ellipseColor, Brushes.Green, 
+                    Brushes.Black, Brushes.Red);
+            }
+            // draw the center dot
+            graphic.DrawString(".", new Font("Georgia", 45), 
+                Brushes.Firebrick, new PointF(180, 135));
+            int[] handCoordinationArray = new int[] { };
+            //draw seconds hand  
+            handCoordinationArray = 
+                MinuteAndSecondCoordination(seconds, secondHand);
+            graphic.DrawLine(new Pen(Color.Red, 4f), 
+                new Point((int)center.X, (int)center.Y),
+                new Point(handCoordinationArray[0], handCoordinationArray[1]));
+            //draw minutes hand  
+            handCoordinationArray = 
+                MinuteAndSecondCoordination(minutes, minuteHand);
+            graphic.DrawLine(new Pen(Color.Green, 5f), 
+                new Point((int)center.X, (int)center.Y),
+                new Point(handCoordinationArray[0], handCoordinationArray[1]));
+            //draw hours hand  
+            handCoordinationArray = 
+                HourCoordination(hours % 12, minutes, hourHand);
+            graphic.DrawLine(new Pen(Color.White, 5f), 
+                new Point((int)center.X, (int)center.Y),
+                new Point(handCoordinationArray[0], handCoordinationArray[1]));
+            return handCoordinationArray;
+        }
+
+        private void PrintTimeZoneText()
+        {
+            firstCountryName.Text = "Bulgarian";
+            firstCountryTime.Text = "time";
+            firstCountryZone.Text = "zone";
+            secondCountryName.Text = "Italian";
+            secondCountryTime.Text = "time";
+            secondCountryZone.Text = "zone";
+        }
+
+        private void DrawRomanNumerals(Color EllipseColor, Brush firstFlagColor, 
+            Brush secondFlagColor, Brush thirdFlagColor)
+        {
+            //draw a circle  
+            graphic.DrawEllipse(new Pen(EllipseColor, 6f), 0, 0, width, height);
+            //draw clock roman numerals  
+            graphic.DrawString("I", new Font("Georgia", 13), secondFlagColor, new PointF(280, 35));
+            graphic.DrawString("II", new Font("Georgia", 13), thirdFlagColor, new PointF(339, 100));
+            graphic.DrawString("III", new Font("Georgia", 13), firstFlagColor, new PointF(356, 188));
+            graphic.DrawString("IV", new Font("Georgia", 13), secondFlagColor, new PointF(335, 275));
+            graphic.DrawString("V", new Font("Georgia", 13), thirdFlagColor, new PointF(282, 340));
+            graphic.DrawString("VI", new Font("Georgia", 13), firstFlagColor, new PointF(185, 365));
+            graphic.DrawString("VII", new Font("Georgia", 13), secondFlagColor, new PointF(94, 340));
+            graphic.DrawString("VIII", new Font("Georgia", 13), thirdFlagColor, new PointF(33, 275));
+            graphic.DrawString("IX", new Font("Georgia", 13), firstFlagColor, new PointF(11, 188));
+            graphic.DrawString("X", new Font("Georgia", 13), secondFlagColor, new PointF(38, 100));
+            graphic.DrawString("XI", new Font("Georgia", 13), thirdFlagColor, new PointF(96, 38));
+            graphic.DrawString("XII", new Font("Georgia", 13), firstFlagColor, new PointF(180, 10));
+        }
+
+        private PathGradientBrush GradientBrushFill(Color centerColor, 
+            Color[] surroundColor)
+        {
+            GraphicsPath graphicsPath = new GraphicsPath();
+            graphicsPath.AddEllipse(0, 0, width, height);
+            PathGradientBrush pathGradientBrush = 
+                new PathGradientBrush(graphicsPath);
+            // set the center gradient Brush color. 
+            pathGradientBrush.CenterColor = centerColor;
+            // set the surround gradient Brush color. 
+            pathGradientBrush.SurroundColors = surroundColor;
+            return pathGradientBrush;
+        }
+
+        //method for drawing lines, representing seconds/minutes.
+        private void PropertyLines(Color color, Color secondColor)
+        {
+            for (int index = 0; index < 60; index++)
+            {
+                // create lines between the hour roman numerals
+                // each line indicates seconds/minutes.
+                if (index % 5 != 0)
+                {
+                    graphic.DrawLine(new Pen(color, radius * 0.022f),
+                         (float)Math.Sin(index * 6 * Math.PI / 180) * radius * 0.95f + center.Y,
+                         (float)Math.Cos(index * 6 * Math.PI / 180) * radius * 0.95f + center.X,
+                         (float)Math.Sin(index * 6 * Math.PI / 180) * radius + center.Y,
+                         (float)Math.Cos(index * 6 * Math.PI / 180) * radius + center.X);
+                }
+                // create space into the roman numerals' position.
+                else if (index % 5 == 0)
+                {
+                    graphic.DrawLine(new Pen(secondColor, size * 0.04f),
+                        (float)Math.Sin(index * 6 * Math.PI / 180) * size * 0.95f + center.Y,
+                        (float)Math.Cos(index * 6 * Math.PI / 180) * size * 0.95f + center.X,
+                        (float)Math.Sin(index * 6 * Math.PI / 180) * size + center.Y,
+                        (float)Math.Cos(index * 6 * Math.PI / 180) * size + center.X);
+                }
+            }
+        }
+
+        private int[] MinuteAndSecondCoordination(int value, int minuteOrSecondLength)
+        {
+            int[] coordinationCenterArray = new int[2];
+            value *= 6; // note: each minute and seconds make a 6 degree  
+            if (value >= 0 && value <= 100)
+            {
+                coordinationCenterArray[0] = 
+                    (int)center.X + (int)(minuteOrSecondLength * Math.Sin(Math.PI * value / 180));
+                coordinationCenterArray[1] = 
+                    (int)center.Y - (int)(minuteOrSecondLength * Math.Cos(Math.PI * value / 180));
+            }
+            else
+            {
+                coordinationCenterArray[0] = 
+                    (int)center.X - (int)(minuteOrSecondLength * -Math.Sin(Math.PI * value / 180));
+                coordinationCenterArray[1] = 
+                    (int)center.Y - (int)(minuteOrSecondLength * Math.Cos(Math.PI * value / 180));
+            }
+            return coordinationCenterArray;
+        }
+
+        private int[] HourCoordination(int hourvalue, int minuteValue, int hourLength)
+        {
+            int[] coordinationCenterArray = new int[2];
+            //each hour makes 30 degree with min making 0.5 degree  
+            int value = (int)((hourvalue * 30) + (minuteValue * 0.5));
+            if (value >= 0 && value <= 180)
+            {
+                coordinationCenterArray[0] = 
+                    (int)center.X + (int)(hourLength * Math.Sin(Math.PI * value / 180));
+                coordinationCenterArray[1] = 
+                    (int)center.Y - (int)(hourLength * Math.Cos(Math.PI * value / 180));
+            }
+            else
+            {
+                coordinationCenterArray[0] = 
+                    (int)center.X - (int)(hourLength * -Math.Sin(Math.PI * value / 180));
+                coordinationCenterArray[1] = 
+                    (int)center.Y - (int)(hourLength * Math.Cos(Math.PI * value / 180));
+            }
+            return coordinationCenterArray;
+        }
+
+        private void DigitalClock(int seconds, int minutes, int hours)
+        {
+            // create objects from StringBuilder class.
+            StringBuilder digitalTimeNowBG = new StringBuilder();
+            StringBuilder digitalTimeNowItaly = new StringBuilder();
+            // condition for appending hours in the variables
+            // it represents hours for the digital clocks
+            if (hours < 10)
+            {
+                digitalTimeNowBG.Append("0" + hours);
+                // italy zero hour Condition
+                if (hours == 0)
+                {
+                    digitalTimeNowItaly.Append(23);
+                }
+                else
+                {
+                    digitalTimeNowItaly.Append("0" + (hours - 1));
+                }
+            }
+            else
+            {
+                digitalTimeNowBG.Append(hours);
+                digitalTimeNowItaly.Append(hours - 1);
+            }
+            digitalTimeNowBG.Append(":");
+            digitalTimeNowItaly.Append(":");
+            // condition for appending minutes in the variables
+            // it represents minutes for the digital clocks
+            if (minutes < 10)
+            {
+                digitalTimeNowBG.Append("0" + minutes);
+                digitalTimeNowItaly.Append("0" + minutes);
+            }
+            else
+            {
+                digitalTimeNowBG.Append(minutes);
+                digitalTimeNowItaly.Append(minutes);
+            }
+            // set the time for the digitalClockBG lable
+            digitalClockBG.Text = digitalTimeNowBG.ToString();
+            // set the time for the digitalTimeNowItaly lable
+            digitalClockItaly.Text = digitalTimeNowItaly.ToString();
+            // method DigitalClockSeconds sets the value in digitalSeconds
+            StringBuilder digitalSeconds = DigitalClockSeconds(seconds);
+            // set seconds for both countries.
+            secondsDigClockBG.Text = digitalSeconds.ToString();
+            secondsDigClockItaly.Text = digitalSeconds.ToString();
+        }
+
+        private StringBuilder DigitalClockSeconds(int seconds)
+        {
+            // create object from class StringBuilder
+            // it represents seconds for the digital clocks
+            StringBuilder digitalSeconds = new StringBuilder();
+            // condition for appending seconds in the variable
+            // it represents seconds for the digital clocks
+            if (seconds < 10)
+            {
+                digitalSeconds.Append("0" + seconds);
+            }
+            else
+            {
+                digitalSeconds.Append(seconds);
+            }
+            return digitalSeconds;
+        }
+    }
+}
